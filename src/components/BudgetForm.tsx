d1f42dbe258fb75470874d8deac07db6a14f923e
agu.banchi <au.banchi@gmail.comgit config --global user.email agu.banchi@gmail.com>
2026-07-14 10:06:15 -0300

    generando formulario para definir presupuesto

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,6 +1,43 @@
 
+
+
+import { useMemo, useState } from "react"
+
 export default function BudgetForm() {
-  return (
-    <div>BudgetForm</div>
+  
+  const [budget, setBudget] = useState(0)
+  
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>)=>{
+        
+        setBudget(e.target.valueAsNumber)
+    }
+
+    const isValid = useMemo(() => {
+        return isNaN (budget) || budget <= 0 ? false : true
+        
+    }, [budget])
+
+    return (
+    <form className="space-y-5">
+<div className="flex flex-col space-y-5">
+<label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+    Definir Presupuesto
+</label>
+</div>
+<input type="number"
+id="budget"
+name="budget" 
+value={budget}
+onChange={handleChange}
+className="w-full bg-white border border-gray-200 p-2"/>
+    
+    <input type="submit"
+    value="Definir Presupuesto"
+    className="bg-blue-600 hover:bg-blue-700 uppercase cursor-pointer w-full p-2 text-white font-black disabled:opacity-40" 
+    disabled={!isValid}/>
+    
+    
+    </form>
   )
 }
